862f8c8764257f4d29f319d66d0a1414c2f7de60
wjuma19dev2026-wq <wjuma19dev2026@gmail.com>
2026-03-11 14:14:02 -0400

    Modificada la variable DefaultScore por solo Score

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-let defaultScore = 20
-
+let body = document.querySelector('body')
 let messageBox = document.querySelector('.message')
 let scoreBox = document.querySelector('.score')
+let highscoreBox = document.querySelector('.highscore')
 let number = document.querySelector('.number')
 let btnCheck = document.querySelector('.btn.check')
+
+let score = 20
+let highScore = 0
 
 /**
  * Input para ingresar el valor numerico a validar
@@ -18,7 +21,7 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1
 number.textContent = secretNumber
 // scoreBox.textContent = '10'
 // inputGuess.value = 13
-scoreBox.textContent = defaultScore
+scoreBox.textContent = score
 
 // inputGuess.addEventListener('click', inputGuessProcess)
 
@@ -27,22 +30,38 @@ btnCheck.addEventListener('click', () => {
 
   if (!guess) {
     messageBox.textContent = ' ⚠ Opss! esto no es un numero'
-    return
+    //  FIXME: Change body background color when Not input Value
   } else if (guess === secretNumber) {
     messageBox.textContent = '🎉 Correct Number!'
+
+    //  FIXME: Change body background color when Win
+    number.style.width = '30rem'
+    body.removeAttribute('class')
+    body.setAttribute('class', 'bg-success')
+
+    if (score > highScore) {
+      highScore = score
+      highscoreBox.textContent = highScore
+    }
   } else if (guess > secretNumber) {
-    if (defaultScore > 1) {
+    if (score > 1) {
       messageBox.textContent = '📉 Too high!'
-      defaultScore--
-      scoreBox.textContent = defaultScore
-      return
+      score--
+      scoreBox.textContent = score
+      number.style.width = '15rem'
+      body.removeAttribute('class')
+      body.setAttribute('class', 'bg-dark')
     } else {
       messageBox.textContent = 'You lost the game'
       scoreBox.textContent = 0
+      //  FIXME: Change body background color when Loose
+      number.style.width = '15rem'
+      body.removeAttribute('class')
+      body.setAttribute('class', 'bg-danger')
     }
   } else if (guess < secretNumber) {
     messageBox.textContent = '📈 Too low!'
-    defaultScore--
-    scoreBox.textContent = defaultScore
+    score--
+    scoreBox.textContent = score
   }
 })
